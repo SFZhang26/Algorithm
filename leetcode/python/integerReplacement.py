a1d41dd0da89a45545b84482ce76d9d3a47e5abb
sfzhang <sf_zhang26@163.com>
2017-03-08 13:25:24 +0800

    leetcode 397

---
+++ leetcode/python/integerReplacement.py
@@ -0,0 +1,15 @@
+# 397. Integer Replacement
+
+class Solution(object):
+    def integerReplacement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        if n==1:
+            return 0
+        if n&1:
+            return min(self.integerReplacement(n+1),self.integerReplacement(n-1))+1
+        else:
+            return self.integerReplacement(n/2)+1
