157f63a0ffffa1d74c8116bf70cff7278a64234f
sf zhang <sf_zhang26@163.com>
2017-03-10 12:30:24 +0800

    hihocoder 1128 updated

--- hihocoder/no1128.cpp
+++ hihocoder/no1128.cpp
@@ -15,25 +15,38 @@ using namespace std;
 
 typedef long long ll;
 
-int solve(vector<int> nums,int target){
-	sort(nums.begin(), nums.end());
-	int left = 0, right = nums.size() - 1;
-	while (left <= right) {
-		int mid = left + (right - left) / 2;
-		if (nums[mid] == target) return mid + 1;
-		else if (nums[mid] > target) right = mid - 1;
-		else left = mid + 1;
+int T;
+
+int helper(vector<int> nums, int left, int right,int target) {
+	if (left >= right) return 1;
+	int i = left, j = right;
+	int pivot = nums[left];
+	while (i < j) {
+		while (i < j&&nums[j] > pivot) j--;
+		nums[i] = nums[j];
+		while (i < j&&nums[i] < pivot) i++;
+		nums[j] = nums[i];
 	}
-	return -1;
+	nums[i] = pivot;
+	if (target == pivot) return i - left + 1;
+	else if (target>pivot) return i - left + 1 + helper(nums, i + 1, right, target);
+	else return helper(nums, left, i - 1, target);
+}
+
+int solve(int n, int k) {
+	vector<int> vec(n, 0);
+	bool flag = false;
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i];
+		if (vec[i] == k) flag = true;
+	}
+	if (!flag) return -1;
+	else return helper(vec, 0, n - 1, k);
 }
 
 int main() {
 	int n, k;
 	cin >> n >> k;
-	vector<int> vec(n, 0);
-	for (int i = 0; i < n; i++) cin >> vec[i];
-	int reuslt = solve(vec, k);
-	cout << reuslt << endl;
-
+	cout << solve(n, k) << endl;
 	return 0;
 }
