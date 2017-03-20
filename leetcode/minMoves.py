4b634f698c62720a59616b891de058d6a22aa4b3
sfzhang <sf_zhang26@163.com>
2017-03-20 10:24:21 +0800

    leetcode 453

---
+++ leetcode/minMoves.py
@@ -0,0 +1,9 @@
+# 453. Minimum Moves to Equal Array Elements
+
+class Solution(object):
+    def minMoves(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(nums)-len(nums)*min(nums)
