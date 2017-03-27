dae4bb2b3a07356e52a5d309aa0bc83127e3d7ce
sfzhang <sf_zhang26@163.com>
2017-03-27 15:23:35 +0800

    leetcode 413

---
+++ leetcode/numberOfArithmeticSlices.py
@@ -0,0 +1,16 @@
+# 413. Arithmetic Slices
+
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        if(len(A)<3):
+            return 0
+        dp=[0]*len(A)
+        for i in range(2,len(A)):
+            if(A[i]-A[i-1]==A[i-1]-A[i-2]):
+                dp[i]=dp[i-1]+1
+        
+        return sum(dp)
